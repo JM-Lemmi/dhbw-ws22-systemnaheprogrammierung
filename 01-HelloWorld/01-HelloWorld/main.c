7f333dc7b8b00fc8a8fc364ea60c825f074a6ebe
Julian Lemmerich <41118534+JM-Lemmi@users.noreply.github.com>
2022-11-25 11:43:54 +0100

    Hello World Boilerplate

---
+++ 01-HelloWorld/01-HelloWorld/main.c
@@ -0,0 +1,17 @@
+/*
+ * 01-HelloWorld.c
+ *
+ * Created: 25/11/2022 11:10:22
+ * Author : Julian Lemmerich
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
